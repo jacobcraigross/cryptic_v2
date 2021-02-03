edc6831d1f3546151fae23a8d62859adfc8cb41c
admin <admin@admins-MBP.attlocal.net>
2021-02-03 09:38:24 -0600

    update 2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>Coingecko API</h1>
+      <a href="www.coingecko.com">coin gecko</a>
     </div>
   );
 }
