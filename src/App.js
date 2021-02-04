99d25d8378bd428214fcbbd3bfac0410cbb6267a
admin <admin@admins-MBP.attlocal.net>
2021-02-04 06:02:47 -0600

    now displaying API data to the screen

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import React, {useState, useEffect} from 'react';
 import Axios from 'axios';
 import './App.css';
 import axios from 'axios';
+import Coin from './Coin';
 
 
 
 function App() {
   // common pattern 
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
 
   // useEffect for the Axios / coingecko API / .then for the promise after 
   useEffect(() => {
@@ -21,17 +23,30 @@ function App() {
     .catch(error => console.log(error));
   }, []);
 
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
 
-  // about to start creating the JSX video ends at 13:11 !!!!!!!
+  const filteredCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()));
+
   return (
     <div className="coin-app">
       <div className="coin-search">
         <h1 className="coin-text">search a currency</h1>
         <form>
-          <input type="text" placeholder="search" className="coin-input"/>
+          <input type="text" placeholder="search" className="coin-input" onChange={handleChange}/>
         </form>
       </div>
-      
+      {filteredCoins.map(coin => {
+        return(<Coin 
+          key={coin.id} 
+          name={coin.name} 
+          image={coin.image} 
+          symbol={coin.symbol} 
+          volume={coin.market_cap}
+          price={coin.current_price}
+          />)
+      })}
     </div>
   );
 }
